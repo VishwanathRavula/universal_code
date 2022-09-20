a6d2c1567529e7721aecc61317d0eb8d0a155d87
VishwanathRavula <vishwaforever5@gmail.com>
2022-09-20 15:40:16 +0530

    new changes

--- time_diff.py
+++ time_diff.py
@@ -6,17 +6,15 @@ st = time.time()
 # main program
 # find sum to first 1 million numbers
 
-exe_file_path = "C:/Users/Dimaag-2022/Desktop/Projects/ngtransfer_V3.2.7_exe/ngtransfer_V3.2.7/ngtransfer_V3.2.7.exe"
+exe_file_path = "" # file path of exe file
 
-exe_file_path = "C:/Users/Dimaag-2022/Desktop/Projects/ngtransfer_V3.2.6_gpu/ngtransfer_V3.2.6_multipleNgPins.py"
+original = "\"home/ex\"" # parameters
 
-original = "\"C:/Users/Dimaag-2022/Desktop/Projects/ngtransfer_V3.2.7_exe/sample_test_ngTransfer/0000000333/Sop/Component/0000000000000000/00000011000000000000005\""
+ngpin = "\"home/ex\""#paramteres
 
-ngpin = "\"C:/Users/Dimaag-2022/Desktop/Projects/ngtransfer_V3.2.7_exe/sample_test_ngTransfer/ng_pins_paths.csv\""
 
-tpe = "sop"
 
-command = f" python {exe_file_path} --ok_comp {original} --ok_pins_csv {original}/pins.csv  --ng_comps_path {ngpin} --outdir \"C:/Users/Dimaag-2022/Downloads/results-6thmay/output/result_ng\"  --type  \"sop\" --shape"
+command = f" python {exe_file_path} --ok_comp {original} --ok_pins_csv {original}/pins.csv  --ng_comps_path {ngpin} --outdir \"C:/Users/Downloads/results-6thmay/output/result_ng\"  --type  \"\" --shape"
 # print(command)
 os.system(command)  
 
